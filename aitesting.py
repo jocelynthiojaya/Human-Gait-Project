41a87e132fbbd34b44b67793fc7958130a5d65f2
jocelynthiojaya <jocelynthiojaya@gmail.com>
2022-10-04 15:23:54 +0700

    move imports to the top

--- aitesting.py
+++ aitesting.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 import csv
-videos_data = pd.read_csv("csvoutput/jocelyn.csv")
+
+videos_data = pd.read_csv("csvoutput/test2.csv")
 videos_data.to_numpy()
 
 list = []
@@ -15,9 +17,9 @@ for i in x:
     output = eval(i)
     list.append(output)
     
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(list, y, test_size = 0.20)
-from sklearn.svm import SVC
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
+
+print(list[0])
