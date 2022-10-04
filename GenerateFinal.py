6d1832bbfa5ae01e54337c7ac41543d5248d539e
jocelynthiojaya <jocelynthiojaya@gmail.com>
2022-10-04 14:34:17 +0700

    updated comments for GenerateFinal.py

--- GenerateFinal.py
+++ GenerateFinal.py
@@ -15,19 +15,12 @@ for file in os.listdir(directory):
 
     if filename.endswith(".csv"):
 
+        # Load csv into dataframe
         df = pd.read_csv('csvoutput/' + filename)
-        #print(df)
 
+        # Determine the gender
         gender = df.iloc[0][6]
-        #print(gender)
 
-        # for row in df.index:
-        #     for col in range(5):
-        #         single_frame.append(df.iloc[row][col+1])
-        #         #print(angles_list)
-        #     all_frames.append(single_frame)
-        #     single_frame = []
-        #print(all_frames)
         df_angles = df[["r_hip_ang", "r_knee_ang", "r_ankle_ang", "r_elbow_ang", "r_shoulder_ang"]]
         all_angles = df_angles.values.tolist()
 
