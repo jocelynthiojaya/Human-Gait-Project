ac0d2ae5819b8067bf9c69bd2c82d3cd7127c4f9
jocelynthiojaya <jocelynthiojaya@gmail.com>
2022-10-05 14:48:03 +0700

    update GenerateFinal to include min and max angles

--- GenerateFinal.py
+++ GenerateFinal.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 import os
+import numpy as np
 
 directory = os.fsencode("csvoutput")
 
@@ -11,7 +12,6 @@ for file in os.listdir(directory):
  
     single_video = []
     all_angles = []
-    #single_frame = []
 
     if filename.endswith(".csv"):
 
@@ -21,23 +21,54 @@ for file in os.listdir(directory):
         # Determine the gender
         gender = df.iloc[0][6]
 
+        # Load dataframe with the wanted headers
         df_angles = df[["r_hip_ang", "r_knee_ang", "r_ankle_ang", "r_elbow_ang", "r_shoulder_ang"]]
+
+        # Generate 1 dim list for angles
         all_angles = df_angles.values.flatten()
         all_angles = all_angles[:100].tolist()
 
+        # Generate min angles
+        min_r_hip_ang = df_angles["r_hip_ang"].min()
+        min_r_knee_ang = df_angles["r_knee_ang"].min()
+        min_r_ankle_ang = df_angles["r_ankle_ang"].min()
+        min_r_elbow_ang = df_angles["r_elbow_ang"].min()
+        min_r_shoulder_ang = df_angles["r_shoulder_ang"].min()
+
+        # Generate max angles
+        max_r_hip_ang = df_angles["r_hip_ang"].max()
+        max_r_knee_ang = df_angles["r_knee_ang"].max()
+        max_r_ankle_ang = df_angles["r_ankle_ang"].max()
+        max_r_elbow_ang = df_angles["r_elbow_ang"].max()
+        max_r_shoulder_ang = df_angles["r_shoulder_ang"].max()
+
+        # Creating list for a single video
         single_video.append(all_angles)
         single_video.append(gender)
         single_video.append(filename)
 
+        single_video.append(min_r_hip_ang)
+        single_video.append(min_r_knee_ang)
+        single_video.append(min_r_ankle_ang)
+        single_video.append(min_r_elbow_ang)
+        single_video.append(min_r_shoulder_ang)
+        
+        single_video.append(max_r_hip_ang)
+        single_video.append(max_r_knee_ang)
+        single_video.append(max_r_ankle_ang)
+        single_video.append(max_r_elbow_ang)
+        single_video.append(max_r_shoulder_ang)
+
+        # Put everything into the final csv
         final_list.append(single_video)
 
-header = ['angles', 'gender', 'filename']
-with open('test2.csv', 'w', newline = '') as file:
+# Make header for final csv
+header = ['angles', 'gender', 'filename', 
+        'min_r_hip_ang', 'min_r_knee_ang', 'min_r_ankle_ang', 'min_r_elbow_ang', 'min_r_shoulder_ang'
+        'max_r_hip_ang', 'max_r_knee_ang', 'max_r_ankle_ang', 'max_r_elbow_ang', 'max_r_shoulder_ang']
+
+with open('test3.csv', 'w', newline = '') as file:
     writer = csv.writer(file)
     writer.writerow(header)
     writer.writerows(final_list)
         
-
-#print(final_list)
-
-    
