c65ad0a9d194b72d80bc468a0fdfb175827447aa
kronmess <57931505+kronmess@users.noreply.github.com>
2022-10-07 14:25:07 +0700

    added other svm code for later reference

--- aitesting.py
+++ aitesting.py
@@ -7,7 +7,7 @@ from sklearn import metrics
 import csv
 from ast import literal_eval
 
-videos_data = pd.read_csv("csvoutput/test3.csv", index_col=False)
+videos_data = pd.read_csv("csvoutput/femaletest.csv", index_col=False)
 list = []
 x = videos_data.drop(['gender', 'filename'], axis = 1)
 
@@ -18,14 +18,18 @@ for i in x['angles'].values:
     list.append(output)
     
 x['angles'] = list
+x_new = np.array(x['angles'].values.tolist())
 #print(x.head())
+x['angles'] = np.vstack(x['angles'])
+# x['angles'] = x_new
+# print(x['angles'])
 
-X_train, X_test, y_train, y_test = train_test_split(list, y, test_size = 0.20)
-svclassifier = SVC(kernel='rbf')
-svclassifier.fit(X_train, y_train)
-y_pred = svclassifier.predict(X_test)
+# X_train, X_test, y_train, y_test = train_test_split(list, y, test_size = 0.20)
+# svclassifier = SVC(kernel='rbf')
+# svclassifier.fit(X_train, y_train)
+# y_pred = svclassifier.predict(X_test)
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+# print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 # from sklearn.metrics import classification_report, confusion_matrix
 # print(confusion_matrix(y_test,y_pred))
