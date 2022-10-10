bb43a9a18ca0c6b178b677e6f7e47ba14e7d5bf6
jocelynthiojaya <jocelynthiojaya@gmail.com>
2022-10-10 15:12:14 +0700

    create VelocityAccel.py

--- ConcatFrames.py
+++ ConcatFrames.py
@@ -2,18 +2,18 @@ import pandas as pd
 import csv
 import os
 
-directory = os.fsencode("csvoutput")
+directory = os.fsencode("csvoutputfinal")
 
 li = []
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
  
-    df = pd.read_csv('csvoutput/' + filename, index_col=None, header=0)
+    df = pd.read_csv('csvoutputfinal/' + filename, index_col=None, header=0)
     li.append(df)
 
 final_frame = pd.concat(li, axis=0, ignore_index=True)
 
-#print(frame)
+print(final_frame)
 
 final_frame.to_csv('femalewalk.csv', index=False)
