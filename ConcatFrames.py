0f2b9ca2fc5205a428560b30cc651dd24bd89bb2
kronmess <57931505+kronmess@users.noreply.github.com>
2022-10-12 19:37:06 +0700

    added hyperparameter tuning function

--- ConcatFrames.py
+++ ConcatFrames.py
@@ -2,18 +2,18 @@ import pandas as pd
 import csv
 import os
 
-directory = os.fsencode("csvoutputfinal")
+directory = os.fsencode("csvoutput")
 
 li = []
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
  
-    df = pd.read_csv('csvoutputfinal/' + filename, index_col=None, header=0)
+    df = pd.read_csv('csvoutput/' + filename, index_col=None, header=0)
     li.append(df)
 
 final_frame = pd.concat(li, axis=0, ignore_index=True)
 
-print(final_frame)
+#print(frame)
 
 final_frame.to_csv('femalewalk.csv', index=False)
