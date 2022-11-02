f0cf2d02f5f141b7beea1e34fbef96943b3aaf59
jocelynthiojaya <jocelynthiojaya@gmail.com>
2022-11-02 12:57:57 +0700

    added data for hanny and jason

--- ConcatFrames.py
+++ ConcatFrames.py
@@ -2,18 +2,18 @@ import pandas as pd
 import csv
 import os
 
-directory = os.fsencode("csvoutput")
+directory = os.fsencode("csv")
 
 li = []
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
  
-    df = pd.read_csv('csvoutput/' + filename, index_col=None, header=0)
+    df = pd.read_csv('csv/' + filename, index_col=None, header=0)
     li.append(df)
 
 final_frame = pd.concat(li, axis=0, ignore_index=True)
 
 #print(frame)
 
-final_frame.to_csv('femalewalk.csv', index=False)
+final_frame.to_csv('csv/walk.csv', index=False)
