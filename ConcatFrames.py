684eb07fd25a0a3c51e624e0f4ee7179494e1781
jocelynthiojaya <jocelynthiojaya@gmail.com>
2023-01-25 22:20:04 +0700

    new testingmodels

--- ConcatFrames.py
+++ ConcatFrames.py
@@ -2,18 +2,19 @@ import pandas as pd
 import csv
 import os
 
-directory = os.fsencode("csv")
+folder = "temp"
+directory = os.fsencode(folder)
 
 li = []
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
  
-    df = pd.read_csv('csv/' + filename, index_col=None, header=0)
+    df = pd.read_csv(folder + '/' + filename, index_col=None, header=0)
     li.append(df)
 
 final_frame = pd.concat(li, axis=0, ignore_index=True)
 
 #print(frame)
 
-final_frame.to_csv('csv/walk.csv', index=False)
+final_frame.to_csv(folder + '/walk.csv', index=False)
